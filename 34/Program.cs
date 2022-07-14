9b61d40f7c462456316c9d060acae02816a3476e
Nataly <17pn@mail.ru>
2022-07-14 17:13:38 +0300

    задание 5

--- 34/Program.cs
+++ 34/Program.cs
@@ -2,3 +2,15 @@
 
 [345, 897, 568, 234] -> 2*/
 
+
+int N = 6;
+int [] arr = new int [N];
+Random rnd = new Random();
+int sum = 0;
+
+for (int i = 0; i < N; i++); {
+    arr[i] = rnd.Next (100, 1000);
+    if (i % 2) {
+        sum += arr[i];
+    }
+}
--- 38/Program.cs
+++ 38/Program.cs
@@ -2,3 +2,21 @@
 
 [3 7 22 2 78] -> 76*/
 
+int N = 6;
+double [] arr = new double [N];
+Random rnd = new Random();
+double diff = 0;
+
+double max = arr[0]; double min = arr[0];
+for (int i = 0; i < N; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+    if (arr[i] < min) {
+        min = arr[i];
+    }
+}
+diff = max - min;
+
+Console.WriteLine(rnd);
+Console.WriteLine(diff);
